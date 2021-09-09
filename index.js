e7c4418efae6ede2c91a314ac6eb4c2f8c0fb7c5
Sarah <36081518+SarahBehairy@users.noreply.github.com>
2021-09-09 14:59:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const div = document.getElementsByClassName('dropdown-content');
+const clickedImgIndex =1;
+const image = document.querySelectorAll('img');
+const modals = document.querySelectorAll('div.img-wrap');
+const largeImg = document.getElementById("large-img");
+
 function openDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
@@ -15,33 +21,25 @@ function openDropdown() {
     }
   };
 
-  
-var div = document.getElementsByClassName('dropdown-content');
-var clickedImgIndex =1;
+ 
 for(var  i =0;i<div.length;i++){
-
-for(var  j =0;j<div[i].children.length;j++){
-
- div[i].children[j].addEventListener('click',function(){
-    var titleDiv = document.querySelector('h1');
-    const p = document.createElement('h1');
-    p.innerHTML = this.innerHTML;
-    p.classList.add('title');   
-    titleDiv.replaceWith(p);
- });
-}
+    for(var  j =0;j<div[i].children.length;j++){
+     div[i].children[j].addEventListener('click',function(){
+        var titleDiv = document.querySelector('h1');
+        const p = document.createElement('h1');
+        p.innerHTML = this.innerHTML;
+        p.classList.add('title');   
+        titleDiv.replaceWith(p);
+     });
+    }
 }
 
-let image = document.querySelectorAll('img');
 for(let i = 0; i < image.length; i++) {
   image[i].addEventListener('click', () => {
       clickedImgIndex = i;
   });
 }
 
-
-const modals = document.querySelectorAll('div.img-wrap');
-var largeImg = document.getElementById("large-img");
 modals.forEach(function(trigger) {
   trigger.addEventListener('click', function(event) {
     event.preventDefault();
